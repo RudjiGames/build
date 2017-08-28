62e84b2dba9ac01d3f8debd4e5dc3d17d4446f9f
DX-MON <dx-mon@users.sourceforge.net>
2017-08-28 15:14:05 +0100

    Uncommented an important build option for GCC based builds for MTrace code bases

--- toolchain.lua
+++ toolchain.lua
@@ -735,9 +735,9 @@ function commonConfig(_filter, _isLib, _isSharedLib, _rappUsed)
 			"-Wunused-value",
 			"-Wundef",
 		}
---		buildoptions_cpp {
---			"-std=c++11",
---		}
+		buildoptions_cpp {
+			"-std=c++11",
+		}
 		links {
 			"rt",
 			"dl",
