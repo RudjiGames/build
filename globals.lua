93d4c1fbc9509609bc2e77341680bb859b980f30
Milos Tosic <milostosic77@gmail.com>
2017-10-18 09:12:15 +0800

    Skip x32 and x64 architectures for winphone/store

--- globals.lua
+++ globals.lua
@@ -84,7 +84,12 @@ function setPlatforms()
 	if actionUsesXcode() then
 		platforms { "Universal" }
 	elseif actionUsesMSVC() then
-		if not (getTargetOS() == "durango") and not (getTargetOS() == "orbis") then -- durango and orbis add their own platforms
+		if  not (getTargetOS() == "durango")	and 
+			not (getTargetOS() == "orbis")		and
+			not (getTargetOS() == "winphone8")	and
+			not (getTargetOS() == "winphone81")	and
+			not (getTargetOS() == "winstore81")	and
+			not (getTargetOS() == "winstore82") then -- these platforms set their own platform config
 			platforms { "x32", "x64" }
 		end
 	else
