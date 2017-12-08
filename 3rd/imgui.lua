533ccfb3e57089e12cb2f1f3758ec0823aa25874
Milos Tosic <milostosic77@gmail.com>
2017-12-08 13:03:48 +0800

    Add missing slash

--- 3rd/imgui.lua
+++ 3rd/imgui.lua
@@ -7,14 +7,14 @@ local params		= { ... }
 local IMGUI_ROOT	= params[1]
 
 local IMGUI_FILES = {
-	IMGUI_ROOT .. "imconfig.h",
-	IMGUI_ROOT .. "imgui.cpp",
-	IMGUI_ROOT .. "imgui.h",
-	IMGUI_ROOT .. "imgui_draw.cpp",
-	IMGUI_ROOT .. "imgui_internal.h",
-	IMGUI_ROOT .. "stb_rect_pack.h",
-	IMGUI_ROOT .. "stb_textedit.h",
-	IMGUI_ROOT .. "stb_truetype.h",
+	IMGUI_ROOT .. "/imconfig.h",
+	IMGUI_ROOT .. "/imgui.cpp",
+	IMGUI_ROOT .. "/imgui.h",
+	IMGUI_ROOT .. "/imgui_draw.cpp",
+	IMGUI_ROOT .. "/imgui_internal.h",
+	IMGUI_ROOT .. "/stb_rect_pack.h",
+	IMGUI_ROOT .. "/stb_textedit.h",
+	IMGUI_ROOT .. "/stb_truetype.h",
 }
 
 function projectAdd_imgui()
