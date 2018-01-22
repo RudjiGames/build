1c80907692dfc6ee3eaf63455d85fba85c9c0d9d
Milos Tosic <milostosic77@gmail.com>
2018-01-22 16:07:28 +0800

    Add jsmn 3rd party module

---
+++ 3rd/jsmn.lua
@@ -0,0 +1,17 @@
+--
+-- Copyright (c) 2017 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+local params		= { ... }
+local JSMN_ROOT		= params[1]
+
+local JSMN_FILES = {
+	JSMN_ROOT .. "jsmn.c",
+	JSMN_ROOT .. "jsmn.h"
+}
+
+function projectAdd_jsmn()
+	addProject_3rdParty_lib("jsmn", JSMN_FILES, false)
+end
+
