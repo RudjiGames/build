f159e3ce7ead1844c8ca82e2d62da6676216b050
milostosic <milostosic77@gmail.com>
2018-07-24 19:56:04 +0800

    Simplify arguments to addProject_lib

--- project_lib.lua
+++ project_lib.lua
@@ -3,7 +3,7 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
-function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _extraIncludes, _extraDefines, _nameAppend)
+function addProject_lib(_name, _libType, _shared, _nameAppend, _disablePCH)
 
 	if _libType == Lib.Tool then
 		group ("toollibs")
@@ -21,7 +21,7 @@ function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _e
 
 		_libType		= _libType or Lib.Runtime
 		_shared			= _shared or false
-		_prebuildcmds	= _prebuildcmds or {}
+		_disablePCH		= _disablePCH or false
 
 		local libKind = "StaticLib"
 		if _shared then
@@ -47,14 +47,6 @@ function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _e
 											{ incFilesPath .. "**.h" } )
 		files  { sourceFiles }
 	
-		if _extraFiles ~= nil then
-			files { _extraFiles }
-		end
-
-		if _extraDefines ~= nil then
-			defines { _extraDefines }
-		end
-
 		local targetOS = getTargetOS()
 		if targetOS == "ios" or targetOS == "osx" then
 			files  { srcFilesPath .. "**.mm" }
@@ -63,7 +55,7 @@ function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _e
 		excludes { projectPath .. "/test/**.*" }
 		excludes { projectPath .. "/samples/**.*" }
 
-		if _extraFiles == nil then
+		if _disablePCH ~= true then
 			addPCH( srcFilesPath, _name )
 		end
 
@@ -77,14 +69,9 @@ function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _e
 
 		addIncludePath(_name, projectPath .. "/3rd/")
 
-		if _extraIncludes ~= nil then
-			includedirs { _extraIncludes }
-		end
-
 		flags { Flags_Libraries }
 		
 		assert(loadfile(RTM_SCRIPTS_DIR .. "configurations.lua"))(	sourceFiles,
-																	_extraConfig,
 																	true,		-- IS_LIBRARY
 																	_shared,	-- IS_SHARED_LIBRARY
 																	false,		-- COPY_QT_DLLS
@@ -92,10 +79,6 @@ function addProject_lib(_name, _libType, _shared, _prebuildcmds, _extraFiles, _e
 																	false,		-- EXECUTABLE
 																	_name
 																	)
-
-		for _,cmd in ipairs( _prebuildcmds ) do
-			prebuildcommands {cmd}
-		end
 
 		addDependencies(project().name)
 end
