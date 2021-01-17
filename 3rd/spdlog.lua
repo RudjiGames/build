160a5fa611e2e89ce52d08049f46a2618430f050
Milos Tosic <milostosic77@gmail.com>
2021-01-17 20:23:34 +0800

    add spdlog

---
+++ 3rd/spdlog.lua
@@ -0,0 +1,29 @@
+--
+-- Copyright (c) 2018 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/gabime/spdlog
+
+local params		= { ... }
+local SPDLOG_ROOT	= params[1]
+
+local SPDLOG_INCLUDE	= {
+	SPDLOG_ROOT .. "include",
+}
+
+local SPDLOG_FILES		= {
+	SPDLOG_ROOT .. "include/*.*",
+	SPDLOG_ROOT .. "src/*.*",
+}
+
+function projectExtraConfig_spdlog()
+	includedirs { SPDLOG_INCLUDE }
+	defines { "SPDLOG_COMPILED_LIB" }
+	configuration "vs*"
+		buildoptions { "/wd 4530" }
+end
+
+function projectAdd_spdlog()
+	addProject_3rdParty_lib("spdlog", SPDLOG_FILES)
+end
