d7e7b4afc30fb4c08be4a58ac2a0377678f63f7d
Milos Tosic <milostosic77@gmail.com>
2021-01-25 12:22:41 +0800

    add mbedtls script

---
+++ 3rd/mbedtls.lua
@@ -0,0 +1,26 @@
+--
+-- Copyright (c) 2018 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/ARMmbed/mbedtls
+
+local params		= { ... }
+local MBEDTLS_ROOT	= params[1]
+
+local MBEDTLS_INCLUDE	= {
+	MBEDTLS_ROOT .. "include",
+}
+
+local MBEDTLS_FILES = {
+	MBEDTLS_ROOT .. "library/**.c",
+}
+
+function projectExtraConfig_mbedtls()
+	includedirs { MBEDTLS_INCLUDE }
+end
+
+function projectAdd_mbedtls()
+	addProject_3rdParty_lib("mbedtls", MBEDTLS_FILES)
+end
+
