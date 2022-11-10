7fa819a06a8072f4e5fdedc901024ea7efc914c8
Milos Tosic <milostosic77@gmail.com>
2022-11-10 21:57:26 +0100

    Update bimg build script

--- 3rd/bimg.lua
+++ 3rd/bimg.lua
@@ -7,35 +7,25 @@
 
 local params	    	= { ... }
 local BIMG_ROOT	    	= params[1]
-local ASTC_CODEC_DIR    = BIMG_ROOT .. "3rdparty/astc-codec/" 
+local ASTC_CODEC_DIR    = BIMG_ROOT .. "3rdparty/astc-encoder/" 
 
 local BIMG_INCLUDE	= {
 	BIMG_ROOT .. "include",
 	BIMG_ROOT .. "3rdparty",
 	BIMG_ROOT .. "3rdparty/iqa/include",
 	BIMG_ROOT .. "3rdparty/tinyexr/deps/miniz",
-	BIMG_ROOT .. "3rdparty/astc-encoder",
-	BIMG_ROOT .. "3rdparty/astc-encoder/include",
+	ASTC_CODEC_DIR,
+	ASTC_CODEC_DIR .. "include",
 	find3rdPartyProject("bx") .. "include"
 }
 
 local BIMG_FILES = {
 	BIMG_ROOT .. "include/**.h",
 	BIMG_ROOT .. "src/**.h",
-    BIMG_ROOT .. "src/**.cpp",
+	BIMG_ROOT .. "src/**.cpp",
 	BIMG_ROOT .. "3rdparty/tinyexr/deps/miniz/**.c",
-
-    ASTC_CODEC_DIR .. "src/decoder/astc_file.*",
-	ASTC_CODEC_DIR .. "src/decoder/codec.*",
-	ASTC_CODEC_DIR .. "src/decoder/endpoint_codec.*",
-	ASTC_CODEC_DIR .. "src/decoder/footprint.*",
-	ASTC_CODEC_DIR .. "src/decoder/integer_sequence_codec.*",
-	ASTC_CODEC_DIR .. "src/decoder/intermediate_astc_block.*",
-	ASTC_CODEC_DIR .. "src/decoder/logical_astc_block.*",
-	ASTC_CODEC_DIR .. "src/decoder/partition.*",
-	ASTC_CODEC_DIR .. "src/decoder/physical_astc_block.*",
-	ASTC_CODEC_DIR .. "src/decoder/quantization.*",
-	ASTC_CODEC_DIR .. "src/decoder/weight_infill.*",
+	ASTC_CODEC_DIR,
+	ASTC_CODEC_DIR .. "source/**.cpp"
 } 
 
 function projectDependencies_bimg()
