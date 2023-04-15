a02e22bfeb769cfcd386310aa89e5627604bfcda
Милош Тошић <milostosic77@gmail.com>
2023-04-15 23:02:33 +0200

    Update qtpresets6.lua

--- qtpresets6.lua
+++ qtpresets6.lua
@@ -245,8 +245,9 @@ function qtConfigure( _config, _projectName, _mocfiles, _qrcfiles, _uifiles, _ts
 			buildoptions { qtFlags }
 			for _,lib in ipairs(_libsToLink) do
 				print("Linking framework: " .. libsDirectory .. "Qt" .. lib .. ".framework")
-				links { libsDirectory .. "Qt" .. lib .. ".framework" }
-				includedirs { libsDirectory .. "Qt" .. lib .. ".framework" }
+				-- links { libsDirectory .. "Qt" .. lib .. ".framework" }
+				-- includedirs { libsDirectory .. "Qt" .. lib .. ".framework" }
+				linkoptions { "-F " .. libsDirectory .. "Qt" .. lib .. ".framework" }
 			end
 		end
 
