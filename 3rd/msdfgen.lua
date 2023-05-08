735d3c831df2815bba03e757420bde6d67535a72
Milos Tosic <milostosic77@gmail.com>
2023-05-08 13:12:06 +0200

    Add freetype2, msdfgen and tinyxml2

---
+++ 3rd/msdfgen.lua
@@ -0,0 +1,23 @@
+--
+-- Copyright (c) 2018 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/Chlumsky/msdfgen
+
+local params		= { ... }
+local MSDFGEN_ROOT	= params[1]
+
+local MSDFGEN_FILES = {
+	MSDFGEN_ROOT .. "core/**.*",
+	MSDFGEN_ROOT .. "ext/**.*",
+}
+
+function projectDependencies_msdfgen()
+	return { "freetype2", "tinyxml2" }
+end 
+
+function projectAdd_msdfgen()
+	addProject_3rdParty_lib("msdfgen", MSDFGEN_FILES)
+end
+
