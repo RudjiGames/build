4780611fef2fc4a9f0cf7a7bcd2c274c77ec35a2
Milos Tosic <milostosic77@gmail.com>
2023-05-27 21:22:41 +0200

    Add efsw, meshoptimizer. Remove NvTriStrip.

---
+++ 3rd/efsw.lua
@@ -0,0 +1,29 @@
+--
+-- Copyright (c) 2023 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/SpartanJ/efsw
+
+local params		= { ... }
+local EFSW_ROOT		= params[1]
+
+local EFSW_INCLUDE	= {
+	EFSW_ROOT .. "include",
+	EFSW_ROOT .. "src",
+}
+
+local EFSW_FILES = {
+	EFSW_ROOT .. "include/**.h",
+	EFSW_ROOT .. "src/**.h",
+	EFSW_ROOT .. "src/**.cpp",
+	EFSW_ROOT .. "src/**.h"
+}
+
+function projectExtraConfig_efsw()
+	includedirs { EFSW_INCLUDE }
+end
+
+function projectAdd_efsw()
+	addProject_3rdParty_lib("efsw", EFSW_FILES)
+end
