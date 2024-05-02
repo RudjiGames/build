ee9bf137c895814a1af6c777b8dee3eeb0ea1daf
IMAGUS\milos <milostosic77@gmail.com>
2024-05-02 20:31:14 +0200

    Add ufbx

---
+++ 3rd/ufbx.lua
@@ -0,0 +1,22 @@
+--
+-- Copyright 2024 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/ufbx/ufbx.git
+
+local params		= { ... }
+local UFBX_ROOT		= params[1]
+
+local UFBX_FILES = {
+	UFBX_ROOT .. "ufbx.c",
+	UFBX_ROOT .. "ufbx.h",
+}
+
+function projectExtraConfig_ufbx()
+	includedirs { UFBX_ROOT .. "Include" }
+end
+
+function projectAdd_ufbx()
+	addProject_3rdParty_lib("ufbx", UFBX_FILES)
+end
