045d6e31dbde763b2bf44575666d34701ae55e65
IMAGUS\milos <milostosic77@gmail.com>
2024-05-10 22:49:12 +0200

    Added ogg & vorbis lib scripts

---
+++ 3rd/ogg.lua
@@ -0,0 +1,30 @@
+--
+-- Copyright 2023 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/xiph/ogg
+
+local params	= { ... }
+local OGG_ROOT	= params[1]
+
+local OGG_FILES = {
+	OGG_ROOT .. "src/**.c",
+	OGG_ROOT .. "src/**.h"
+}
+
+function projectDependencies_ogg()
+	return {}
+end
+
+function projectExtraConfig_ogg()
+	includedirs { OGG_ROOT .. "include" }
+end
+
+function projectExtraConfigExecutable_ogg()
+	includedirs { OGG_ROOT .. "include" }
+end
+
+function projectAdd_ogg()
+	addProject_3rdParty_lib("ogg", OGG_FILES)
+end
---
+++ 3rd/vorbis.lua
@@ -0,0 +1,35 @@
+--
+-- Copyright 2023 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/xiph/vorbis
+
+local params		= { ... }
+local VORBIS_ROOT	= params[1]
+
+local VORBIS_FILES = {
+	VORBIS_ROOT .. "lib/**.c",
+	VORBIS_ROOT .. "lib/**.h"
+}
+
+function projectDependencies_vorbis()
+	return { "ogg" }
+end
+
+function projectExtraConfig_vorbis()
+	defines { "M_PI=(3.1415926536f)" }
+	excludes {	VORBIS_ROOT .. "lib/misc.c",
+				VORBIS_ROOT .. "lib/tone.c",
+				VORBIS_ROOT .. "lib/psytune.c"
+	}
+	includedirs { VORBIS_ROOT .. "include" }
+end
+
+function projectExtraConfigExecutable_vorbis()
+	includedirs { VORBIS_ROOT .. "include" }
+end
+
+function projectAdd_vorbis()
+	addProject_3rdParty_lib("vorbis", VORBIS_FILES)
+end
