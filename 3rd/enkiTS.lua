9cdac1403d72430b0f0e5995d986b05c02e87f6b
Milos Tosic <milostosic77@icloud.com>
2025-06-11 07:32:53 +0200

    Fix warnings

--- 3rd/enkiTS.lua
+++ 3rd/enkiTS.lua
@@ -16,7 +16,7 @@ local ENKITS_FILES = {
 function projectExtraConfig_enkiTS()
  	configuration { "vs*", "windows" }
 		buildoptions { "/wd4100"} -- 4100: 'pETS_': unreferenced formal parameter
-	configuration { "linux or *clang*" }
+	configuration { "linux* or *clang*" }
 		buildoptions {
 			"-Wunused-variable",
 			"-Wunused-function",
