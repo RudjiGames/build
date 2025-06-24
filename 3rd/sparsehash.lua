7d38125239bad4451e25ec20b8da621a95ff1ecf
milos.tosic <milos@rudji.com>
2025-06-24 13:47:03 +0200

    Add sparsehash

---
+++ 3rd/sparsehash.lua
@@ -0,0 +1,35 @@
+--
+-- Copyright 2025 Milos Tosic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+
+-- https://github.com/sparsehash/sparsehash.git
+
+local params		= { ... }
+local SPARSEH_ROOT	= params[1]
+
+local SPARSEH_FILES = {
+	SPARSEH_ROOT .. "src/sparsehash/*.*"
+}
+
+function projectExtraConfig_sparsehash()
+	includedirs { SPARSEH_ROOT .. "src" }
+	if getTargetOS() == "windows" then
+		includedirs { SPARSEH_ROOT .. "src/windows" }	
+	end
+end
+
+function projectExtraConfigExecutable_sparsehash()
+	projectExtraConfig_sparsehash()
+end
+
+function projectHeaderOnlyLib_sparsehash()
+end
+
+function projectAdd_sparsehash()
+	if getTargetOS() ~= "windows" then
+		os.execute("cp " .. SPARSEH_ROOT .. "src/config.h.in " .. SPARSEH_ROOT .. "src/sparsehash/internal/sparseconfig.h")
+	end
+
+	addProject_3rdParty_lib("sparsehash", SPARSEH_FILES)
+end
